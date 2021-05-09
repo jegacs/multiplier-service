1e2a8ac9deda19e0cc05d508afc962959d3c9e68
Gabriel Acosta <joga.acosta@gmail.com>
2021-05-09 11:29:03 -0500

    add error package

---
+++ errors/errors.go
@@ -0,0 +1,10 @@
+package errors
+
+import (
+	"errors"
+)
+
+var (
+	ErrGreaterThanLimit = errors.New("number is greater than upper limit allowed")
+	ErrSmallerThanLimit = errors.New("number is smaller than the lower limit allowed")
+)
